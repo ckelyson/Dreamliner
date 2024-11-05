16b6ff468699dfba240591345b3d7aff3246fd2a
Ckelyson Moura <ckelysonjheymison777@gmail.com>
2024-11-05 19:45:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
